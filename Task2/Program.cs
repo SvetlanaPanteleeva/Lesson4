87f1dfc7fcf5a3aa9dbb3021d66589110cfea4e8
Svetlana <Svetlana@LENOVO-PC>
2023-01-24 18:49:12 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите число");
+            int s1 = 0;
+            int s2 = 1;
+            int i;
+            do
+            {
+                i = Convert.ToInt32(Console.ReadLine());
+                if (i > 0)
+                {
+                    s1 += i;
+                }
+                else if (i < 0)
+                    {
+                    s2 -= i;
+                }
+            }
+            while (i != 0);
+            if (s1 > s2)
+            {
+                Console.WriteLine("Количество положительных чисел большe");
+            }
+            else if (s1 < s2)
+            {
+                Console.WriteLine("Количество отрицательных чисел больше");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите C: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            int n = 0;
+            while (a >= c)
+            {
+                a -= c;
+                k++; 
+            }
+            while (b >= c)
+            {
+                b -= c;
+                n++;
+            }
+            int i = 0;
+            int number = 0;
+            while (i < k)
+            {
+                i++;
+                number += n;
+            }
+            Console.WriteLine("N= {0}", number);
+            Console.ReadKey();
+        }
+    }
+}
